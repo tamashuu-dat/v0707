1dbfe0d88b3e9f2d8ea4673404a5e83ec322ea29
A601-13 <tnk.dat.class@gmail.com>
2022-07-07 13:37:00 +0900

    復習

--- v0707/Form1.cs
+++ v0707/Form1.cs
@@ -12,10 +12,31 @@ namespace v0707
 {
     public partial class Form1 : Form
     {
+        int[] data = new int[3];
+        static Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            data[0] = rand.Next(11);
+            data[1] = rand.Next(11);
+            data[2] = rand.Next(11);
+            MessageBox.Show($"{ data[0] }");
+            MessageBox.Show($"{ data[1] }");
+            MessageBox.Show($"{ data[2] }");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"{data[0] + data[1] + data[2]}");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"{(data[0] + data[1] + data[2]) / 3}");
+        }
     }
 }
